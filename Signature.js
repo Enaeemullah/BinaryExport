8853a2d061f844ad718b4d426ef81a586c769f1b
Naeem Ullah <35742914+Enaeemullah@users.noreply.github.com>
2023-10-23 23:10:22 +0500

    Remove .env

--- Signature.js
+++ Signature.js
@@ -14,29 +14,21 @@ const dbConfig = {
     database: process.env.DB_DATABASE,
 };
 
-// Folder path to save images
-const outputFolder = 'F:\\KCBL-SIGNATURE\\SignatureImages';
+const outputFolder = process.env.OUTPUT_FOLDER;
 
-// Path to the image for missing signatures
 const missingSignatureImage = 'C:\\Users\\naeem.ullah\\Desktop\\Signature\\SignatureNotFound.jpg';
 
-// Image dimensions after resizing
 const imageWidth = 400; // Change to the desired width
 const imageHeight = 200; // Change to the desired height
 
 async function signaturePhotos() {
     try {
-        // Create a connection to the database
         const connection = await mysql.createConnection(dbConfig);
 
-        // Query to retrieve image data and Customer_ID from signaturephoto, ordered by Customer_ID
         const query = 'SELECT Customer_ID, Signature FROM signaturephoto ORDER BY Customer_ID';
         const [rows] = await connection.execute(query);
-
-        // Ensure the output folder exists
         fs.mkdirSync(outputFolder, { recursive: true });
 
-        // Define the starting Customer_ID
         let startingCustomerID = rows[0] ? rows[0].Customer_ID : 1;
 
         for (let index = 0; index < rows.length; index++) {
@@ -47,30 +39,25 @@ async function signaturePhotos() {
             let imageBuffer;
 
             if (signature === null) {
-                // Use the missing signature image for null signatures
                 imageBuffer = fs.readFileSync(missingSignatureImage);
             } else {
-                // Resize the image before saving
                 imageBuffer = await sharp(signature)
                     .resize(imageWidth, imageHeight)
                     .toBuffer();
             }
 
-            // Modify the file naming to include a hyphen
             const paddedCustomerID = `${Customer_ID.toString().padStart(2, '0')}-00`;
             const imagePath = path.join(outputFolder, `${paddedCustomerID}.jpg`);
 
             await fs.promises.writeFile(imagePath, imageBuffer);
             console.log(kleur.green('Image saved on SignaturePhotos Folder:'));
-            console.log(kleur.yellow(imagePath));
+            console.log(kleur.blue(imagePath));
 
-            // Increment the startingCustomerID if there's a gap in Customer_IDs
             if (Customer_ID !== startingCustomerID + index) {
                 startingCustomerID = Customer_ID - index;
             }
         }
 
-        // Close the database connection
         await connection.end();
     } catch (error) {
         console.error('Error:', error);
